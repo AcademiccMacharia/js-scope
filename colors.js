01c3904d059b4a6093369ca73e9441e2730eaacc
AcademiccMacharia <machariab19@gmail.com>
2023-05-12 11:54:44 +0300

    removing unnecessary comments

--- colors.js
+++ colors.js
@@ -1,10 +1,3 @@
-// We have the following arrays : 
-// color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
-// o = ["th","st","nd","rd"]
-// Write a JavaScript program to display the colors in the following way :
-// "1st choice is Blue ."
-// "2nd choice is Green."
-// "3rd choice is Red."
 
 let color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
 let o = ["th","st","nd","rd"];
