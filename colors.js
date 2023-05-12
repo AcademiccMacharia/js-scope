2eaabba0ab623d264fb7661ede33efed4cc17fef
AcademiccMacharia <machariab19@gmail.com>
2023-05-12 11:49:56 +0300

    program to display the colors

---
+++ colors.js
@@ -0,0 +1,26 @@
+// We have the following arrays : 
+// color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+// o = ["th","st","nd","rd"]
+// Write a JavaScript program to display the colors in the following way :
+// "1st choice is Blue ."
+// "2nd choice is Green."
+// "3rd choice is Red."
+
+let color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+let o = ["th","st","nd","rd"];
+
+const writeColor = () => {
+    for (let i = 0; i < color.length; i++) {
+        if (i === 0) {
+            console.log(`${i + 1}${o[1]} choice is ${color[i]}`);
+        } else if (i === 1) {
+            console.log(`${i + 1}${o[2]} choice is ${color[i]}`);
+        } else if (i === 2) {
+            console.log(`${i + 1}${o[3]} choice is ${color[i]}`);
+        } else {
+            console.log(`${i + 1}${o[0]} choice is ${color[i]}`);
+        }
+    }
+}
+
+writeColor();
